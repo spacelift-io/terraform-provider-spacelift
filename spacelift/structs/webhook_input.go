0f7309c41d6b59098d8d95925adc248c99a97f72
Jakub Martin <kubam@spacelift.io>
2020-07-07 11:09:38 +0200

    Fixing graphql creation request

--- spacelift/structs/webhook_input.go
+++ spacelift/structs/webhook_input.go
@@ -4,7 +4,7 @@ import "github.com/shurcooL/graphql"
 
 // WebhooksIntegrationInput represents the input required to create or update a webhook.
 type WebhooksIntegrationInput struct {
-	Enabled  graphql.Boolean `graphql:"enabled"`
-	Endpoint graphql.String  `graphql:"endpoint"`
-	Secret   graphql.String  `graphql:"secret"`
+	Enabled  graphql.Boolean `json:"enabled"`
+	Endpoint graphql.String  `json:"endpoint"`
+	Secret   graphql.String  `json:"secret"`
 }
