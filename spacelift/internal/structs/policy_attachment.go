1ecbd8f5a3c894671bd175663c8b84598944af15
Marcin Wyszynski <marcinw@spacelift.io>
2020-09-29 09:33:51 +0200

    Add custom input to the policy attachment resource (#53)

--- spacelift/internal/structs/policy_attachment.go
+++ spacelift/internal/structs/policy_attachment.go
@@ -2,7 +2,8 @@ package structs
 
 // PolicyAttachment is a single policy attachment embedded in a Policy.
 type PolicyAttachment struct {
-	ID       string `graphql:"id"`
-	StackID  string `graphql:"stackId"`
-	IsModule bool   `graphql:"isModule"`
+	ID          string  `graphql:"id"`
+	StackID     string  `graphql:"stackId"`
+	IsModule    bool    `graphql:"isModule"`
+	CustomInput *string `graphql:"customInput"`
 }
