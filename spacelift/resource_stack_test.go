3f180c3071939147b4823e7acfc76329bdb86d32
Jakub Martin <jakub.wit.martin@gmail.com>
2020-10-26 10:29:20 +0100

    Fix tests.

--- spacelift/resource_stack_test.go
+++ spacelift/resource_stack_test.go
@@ -55,4 +55,51 @@ func TestStackResource(t *testing.T) {
 			},
 		})
 	})
+
+	t.Run("with private worker pool and autoretry", func(t *testing.T) {
+		config := func(description string) string {
+			return fmt.Sprintf(`
+				resource "spacelift_stack" "test" {
+					administrative = true
+					autodeploy     = true
+					autoretry      = true
+					branch         = "master"
+					description    = "%s"
+					labels         = ["one", "two"]
+					name           = "Provider test stack %s"
+					project_root   = "root"
+					repository     = "demo"
+					worker_pool_id = spacelift_worker_pool.test.id
+				}
+				
+				resource "spacelift_worker_pool" "test" {
+					name        = "My first worker pool"
+					description = "test worker pool"
+				}
+			`, description, randomID)
+		}
+
+		testSteps(t, []resource.TestStep{
+			{
+				Config: config("old description"),
+				Check: Resource(
+					"spacelift_stack.test",
+					Attribute("id", StartsWith("provider-test-stack-")),
+					Attribute("administrative", Equals("true")),
+					Attribute("autodeploy", Equals("true")),
+					Attribute("autoretry", Equals("true")),
+					Attribute("branch", Equals("master")),
+					Attribute("description", Equals("old description")),
+					SetEquals("labels", "one", "two"),
+					Attribute("name", StartsWith("Provider test stack")),
+					Attribute("project_root", Equals("root")),
+					Attribute("repository", Equals("demo")),
+				),
+			},
+			{
+				Config: config("new description"),
+				Check:  Resource("spacelift_stack.test", Attribute("description", Equals("new description"))),
+			},
+		})
+	})
 }
