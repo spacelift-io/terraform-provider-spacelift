3a879c1ee5b5a7dfaecc0b91828beabf58e5c1ce
Marcin Wyszynski <marcin.pixie@gmail.com>
2020-11-17 11:59:40 +0100

    'spacelift_current_stack' data source

---
+++ spacelift/data_current_stack.go
@@ -0,0 +1,34 @@
+package spacelift
+
+import (
+	"path"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/pkg/errors"
+
+	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal"
+)
+
+func dataCurrentStack() *schema.Resource {
+	return &schema.Resource{Read: dataCurrentStackRead}
+}
+
+func dataCurrentStackRead(d *schema.ResourceData, meta interface{}) error {
+	var claims jwt.StandardClaims
+
+	if _, _, err := (&jwt.Parser{}).ParseUnverified(meta.(*internal.Client).Token, &claims); err != nil {
+		return errors.Wrap(err, "could not parse client token")
+	}
+
+	if issuer := claims.Issuer; issuer != "spacelift" {
+		return errors.Errorf("unexpected token issuer %s, is this a Spacelift run?", issuer)
+	}
+
+	stackID, _ := path.Split(claims.Subject)
+
+	d.SetId(strings.TrimRight(stackID, "/"))
+
+	return nil
+}
