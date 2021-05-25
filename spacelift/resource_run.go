6f7d0c50a60405ea8514d99ce63e80ec8d16fb9a
Marcin Wyszynski <marcin.pixie@gmail.com>
2021-05-25 22:11:52 +0200

    Import order

--- spacelift/resource_run.go
+++ spacelift/resource_run.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/shurcooL/graphql"
+
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal"
 )
 
