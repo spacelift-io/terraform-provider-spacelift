48f73422af9d79c023c848633cafd6b8bf5a9579
viktor <viktork@spacelift.io>
2021-07-28 13:38:10 +0200

    feature: add data sources for VCS integrations (#198)
    * feature: add data sources for VCS integrations
    
    * docs: update VCS integration data sources descriptions
    
    * docs: generate terraform docs files
    
    * parametrize VCS integration details (env vars) for tests
    
    * fix: pr comments, minor refactoring

---
+++ spacelift/data_gitlab_integration_test.go
@@ -0,0 +1,25 @@
+package spacelift
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	. "github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/testhelpers"
+)
+
+func TestGitlabIntegrationData(t *testing.T) {
+	t.Parallel()
+
+	testSteps(t, []resource.TestStep{{
+		Config: `
+			data "spacelift_gitlab_integration" "test" {}
+		`,
+		Check: Resource(
+			"data.spacelift_gitlab_integration.test",
+			Attribute("api_host", Equals(os.Getenv("SPACELIFT_PROVIDER_TEST_GITLAB_API_HOST"))),
+			Attribute("webhook_secret", Equals(os.Getenv("SPACELIFT_PROVIDER_TEST_GITLAB_WEBHOOK_SECRET"))),
+		),
+	}})
+}
