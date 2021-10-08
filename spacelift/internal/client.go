74494c65aecad40f14a5e2eb234fb585b19fe9bc
Adam Connelly <adam.rpconnelly@gmail.com>
2021-10-08 13:11:17 +0200

    Simplify retry handling (#219)
    I realised that I hadn't noticed the retry handling we already had in place when adding the retryable HTTP client. I've removed the retry code and replaced it with a retry policy. It wraps the default policy that already takes care of 5xx errors as well as 429s.

--- spacelift/internal/client.go
+++ spacelift/internal/client.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/shurcooL/graphql"
@@ -20,37 +19,17 @@ type Client struct {
 }
 
 // Mutate runs a GraphQL mutation.
-func (c *Client) Mutate(ctx context.Context, mutationName string, m interface{}, variables map[string]interface{}) (err error) {
+func (c *Client) Mutate(ctx context.Context, mutationName string, m interface{}, variables map[string]interface{}) error {
 	client := c.client(ctx)
 
-	for i := 1; i <= 3; i++ {
-		err = client.Mutate(ctx, m, variables, graphql.WithHeader("Spacelift-GraphQL-Mutation", mutationName))
-
-		if err == nil || !isRetriable(err) {
-			break
-		}
-
-		time.Sleep(time.Duration(i) * time.Second)
-	}
-
-	return
+	return client.Mutate(ctx, m, variables, graphql.WithHeader("Spacelift-GraphQL-Mutation", mutationName))
 }
 
 // Query runs a GraphQL query.
-func (c *Client) Query(ctx context.Context, queryName string, q interface{}, variables map[string]interface{}) (err error) {
+func (c *Client) Query(ctx context.Context, queryName string, q interface{}, variables map[string]interface{}) error {
 	client := c.client(ctx)
 
-	for i := 1; i <= 3; i++ {
-		err = client.Query(ctx, q, variables, graphql.WithHeader("Spacelift-GraphQL-Query", queryName))
-
-		if err == nil || !isRetriable(err) {
-			break
-		}
-
-		time.Sleep(time.Duration(i) * time.Second)
-	}
-
-	return
+	return client.Query(ctx, q, variables, graphql.WithHeader("Spacelift-GraphQL-Query", queryName))
 }
 
 func (c *Client) client(ctx context.Context) *graphql.Client {
@@ -71,15 +50,4 @@ func (c *Client) client(ctx context.Context) *graphql.Client {
 
 func (c *Client) url() string {
 	return fmt.Sprintf("%s/graphql", c.Endpoint)
-}
-
-func isRetriable(err error) bool {
-	switch typedErr := err.(type) {
-	case *graphql.ResponseError:
-		return true
-	case *graphql.ServerError:
-		return typedErr.StatusCode/100 == 5
-	default:
-		return false
-	}
 }
