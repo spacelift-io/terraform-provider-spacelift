85b312a4eb9083a437f5d216186e20442b6b0612
Marcin Wyszynski <marcin.pixie@gmail.com>
2021-12-09 13:32:45 +0100

    Fix broken example

--- examples/resources/spacelift_azure_integration_attachment/resource.tf
+++ examples/resources/spacelift_azure_integration_attachment/resource.tf
@@ -1,6 +1,6 @@
 # For a stack
 resource "spacelift_azure_integration_attachment" "readonly" {
-  integration_id  = spacelift_azure_integration_attachment.example.id
+  integration_id  = spacelift_azure_integration.example.id
   stack_id        = spacelift_stack.example.id
   write           = false
   subscription_id = "subscription_id"
@@ -8,7 +8,7 @@ resource "spacelift_azure_integration_attachment" "readonly" {
 
 # For a module
 resource "spacelift_azure_integration_attachment" "writeonly" {
-  integration_id  = spacelift_azure_integration_attachment.example.id
+  integration_id  = spacelift_azure_integration.example.id
   stack_id        = spacelift_module.example.id
   read            = false
   subscription_id = "subscription_id"
