5992a8f41fb4ca68cd402120d74dadb35462fe22
Marcin Wyszynski <marcinw@spacelift.io>
2022-05-06 10:39:18 +0200

    Fix AWS role retry (#287)

--- spacelift/resource_aws_role.go
+++ spacelift/resource_aws_role.go
@@ -102,7 +102,7 @@ func resourceAWSRoleCreate(ctx context.Context, d *schema.ResourceData, meta int
 
 	for i := 0; i < 5; i++ {
 		err = resourceAWSRoleSet(ctx, meta.(*internal.Client), ID, d)
-		if err == nil || !strings.Contains(err.Error(), "AccessDenied") || i == 4 {
+		if err == nil || !strings.Contains(err.Error(), "could not assume") || i == 4 {
 			break
 		}
 
