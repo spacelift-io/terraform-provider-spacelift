9b4b9128158393c34a62202e037e20ecd40005ff
michiel de wilde <michieldw@spacelift.io>
2022-10-06 17:38:01 +0200

    Fix scheduled_delete_stack example (#372)

--- examples/resources/spacelift_scheduled_delete_stack/resource.tf
+++ examples/resources/spacelift_scheduled_delete_stack/resource.tf
@@ -3,7 +3,7 @@ resource "spacelift_stack" "k8s-core" {
 }
 
 // at a given timestamp (unix)
-resource "spacelift_scheduled_task" "k8s-core-delete" {
+resource "spacelift_scheduled_delete_stack" "k8s-core-delete" {
   stack_id = spacelift_stack.k8s-core.id
 
   at               = "1663336895"
