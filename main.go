ffc283b77e108a827a1f12f10833fba08c343337
tgkishore <37321826+tgkishore@users.noreply.github.com>
2022-10-14 18:06:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
-package main
+//package main
 
-import (
+//import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift"
-)
+//)
 
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
-var commit = "dev"
-var version = "dev"
+//var commit = "dev"
+//var version = "dev"
 
-func main() {
+//func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: spacelift.Provider(commit, version),
 	})
-}
+//}
 
 
 
