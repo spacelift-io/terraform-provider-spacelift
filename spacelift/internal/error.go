56ab74c1f2cfc085f07cd4efba1f6d5a833caf32
peterdeme <snypox@gmail.com>
2023-02-02 21:40:30 +0100

    If error has extensions, show them in the error message

--- spacelift/internal/error.go
+++ spacelift/internal/error.go
@@ -1,15 +1,48 @@
 package internal
 
 import (
+	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/shurcooL/graphql"
 )
 
 // FromSpaceliftError wraps the error with a helpful message when encountering a Spacelift error.
 // In this case an unauthorized error.
 func FromSpaceliftError(err error) error {
-	if err == nil || !strings.Contains(err.Error(), "unauthorized") {
-		return err
+	if err != nil && strings.Contains(err.Error(), "unauthorized") {
+		return fmt.Errorf("%w - is it an administrative stack in the appropriate space?", err)
 	}
-	return fmt.Errorf("%w - is it an administrative stack in the appropriate space?", err)
+
+	var graphErrs graphql.GraphQLErrors
+	if errors.As(err, &graphErrs) {
+		return parseGraphqlErrors(graphErrs)
+	}
+
+	return err
+}
+
+func parseGraphqlErrors(graphErrs graphql.GraphQLErrors) error {
+	errorParts := make([]string, 0, len(graphErrs))
+	for _, err := range graphErrs {
+		errorPart := err.Message
+
+		if len(err.Extensions) > 0 {
+			errorPart = fmt.Sprintf("%s: %s", errorPart, parseExtensions(err.Extensions))
+		}
+
+		errorParts = append(errorParts, errorPart)
+	}
+
+	return errors.New(strings.Join(errorParts, ", "))
+}
+
+func parseExtensions(ext map[string]interface{}) string {
+	errorParts := make([]string, 0, len(ext))
+	for k, v := range ext {
+		errorParts = append(errorParts, fmt.Sprintf("%s: %v", k, v))
+	}
+
+	return strings.Join(errorParts, ", ")
 }
