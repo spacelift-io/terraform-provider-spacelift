b9c734be7ec24c2fefb32de0e52ba78872b502e4
Piotr Truszkowski <truszkowski@gmail.com>
2023-08-21 16:18:34 +0200

    return the components instead of returning a slice

--- spacelift/resource_stack_dependency_reference.go
+++ spacelift/resource_stack_dependency_reference.go
@@ -53,13 +53,13 @@ func resourceStackDependencyReferenceCreate(ctx context.Context, d *schema.Resou
 		StackDependencyReference structs.StackDependencyReference `graphql:"stackDependenciesAddReference(stackDependencyID: $stackDependencyID, reference: $reference)"`
 	}
 
-	idParts, diags := getStackDependencyIDParts(d)
+	stackID, depID, diags := getStackDependencyIDParts(d)
 	if diags != nil {
 		return diags
 	}
 
 	variables := map[string]interface{}{
-		"stackDependencyID": toID(idParts[1]),
+		"stackDependencyID": toID(depID),
 		"reference": structs.StackDependencyReferenceInput{
 			OutputName: toString(d.Get("output_name")),
 			InputName:  toString(d.Get("input_name")),
@@ -71,7 +71,7 @@ func resourceStackDependencyReferenceCreate(ctx context.Context, d *schema.Resou
 		return diag.Errorf("could not create stack dependency reference: %s", err)
 	}
 
-	d.SetId(path.Join(idParts[0], idParts[1], query.StackDependencyReference.ID))
+	d.SetId(path.Join(stackID, depID, query.StackDependencyReference.ID))
 	d.Set("output_name", query.StackDependencyReference.OutputName)
 	d.Set("input_name", query.StackDependencyReference.InputName)
 	return nil
@@ -86,22 +86,22 @@ func resourceStackDependencyReferenceRead(ctx context.Context, d *schema.Resourc
 		} `graphql:"stack(id: $stack_id)"`
 	}
 
-	idParts, diags := getStackDependencyReferenceIDParts(d)
+	stackID, depID, refID, diags := getStackDependencyReferenceIDParts(d)
 	if diags != nil {
 		return diags
 	}
 
 	variables := map[string]interface{}{
-		"stack_id":      toID(idParts[0]),
-		"dependency_id": toID(idParts[1]),
-		"reference_id":  toID(idParts[2]),
+		"stack_id":      toID(stackID),
+		"dependency_id": toID(depID),
+		"reference_id":  toID(refID),
 	}
 
 	if err := meta.(*internal.Client).Query(ctx, "StackDependenciesReferenceRead", &query, variables); err != nil {
 		return diag.Errorf("could not query for stack dependency reference: %s", err)
 	}
 
-	d.Set("stack_dependency_id", path.Join(idParts[0], idParts[1]))
+	d.Set("stack_dependency_id", path.Join(stackID, depID))
 	d.Set("output_name", query.Stack.Dependency.Reference.OutputName)
 	d.Set("input_name", query.Stack.Dependency.Reference.InputName)
 	return nil
@@ -112,14 +112,14 @@ func resourceStackDependencyReferenceUpdate(ctx context.Context, d *schema.Resou
 		StackDependencyReference structs.StackDependencyReference `graphql:"stackDependenciesUpdateReference(reference: $reference)"`
 	}
 
-	idParts, diags := getStackDependencyReferenceIDParts(d)
+	stackID, depID, refID, diags := getStackDependencyReferenceIDParts(d)
 	if diags != nil {
 		return diags
 	}
 
 	variables := map[string]interface{}{
 		"reference": structs.StackDependencyReferenceUpdateInput{
-			ID:         toID(idParts[2]),
+			ID:         toID(refID),
 			OutputName: toString(d.Get("output_name")),
 			InputName:  toString(d.Get("input_name")),
 			Type:       toString("ENVIRONMENT_VARIABLE"),
@@ -130,7 +130,7 @@ func resourceStackDependencyReferenceUpdate(ctx context.Context, d *schema.Resou
 		return diag.Errorf("could not update stack dependency reference: %s", err)
 	}
 
-	d.Set("stack_dependency_id", path.Join(idParts[0], idParts[1]))
+	d.Set("stack_dependency_id", path.Join(stackID, depID))
 	d.Set("output_name", query.StackDependencyReference.OutputName)
 	d.Set("input_name", query.StackDependencyReference.InputName)
 	return nil
@@ -141,13 +141,13 @@ func resourceStackDependencyReferenceDelete(ctx context.Context, d *schema.Resou
 		StackDependencyReference *structs.StackDependencyReference `graphql:"stackDependenciesDeleteReference(id: $id)"`
 	}
 
-	idParts, diags := getStackDependencyReferenceIDParts(d)
+	_, _, refID, diags := getStackDependencyReferenceIDParts(d)
 	if diags != nil {
 		return diags
 	}
 
 	variables := map[string]interface{}{
-		"id": toID(idParts[2]),
+		"id": toID(refID),
 	}
 
 	if err := meta.(*internal.Client).Mutate(ctx, "StackDependenciesDeleteReference", &query, variables); err != nil {
@@ -158,22 +158,22 @@ func resourceStackDependencyReferenceDelete(ctx context.Context, d *schema.Resou
 	return nil
 }
 
-func getStackDependencyIDParts(d *schema.ResourceData) ([]string, diag.Diagnostics) {
+func getStackDependencyIDParts(d *schema.ResourceData) (string, string, diag.Diagnostics) {
 	stackDependencyID := string(toString(d.Get("stack_dependency_id")))
 
 	idParts := strings.SplitN(stackDependencyID, "/", 2)
 	if len(idParts) != 2 {
-		return nil, diag.Errorf("unexpected stack_dependency_id: %s", stackDependencyID)
+		return "", "", diag.Errorf("unexpected stack_dependency_id: %s", stackDependencyID)
 	}
 
-	return idParts, nil
+	return idParts[0], idParts[1], nil
 }
 
-func getStackDependencyReferenceIDParts(d *schema.ResourceData) ([]string, diag.Diagnostics) {
+func getStackDependencyReferenceIDParts(d *schema.ResourceData) (string, string, string, diag.Diagnostics) {
 	idParts := strings.SplitN(d.Id(), "/", 3)
 	if len(idParts) != 3 {
-		return nil, diag.Errorf("unexpected resource ID: %s", d.Id())
+		return "", "", "", diag.Errorf("unexpected resource ID: %s", d.Id())
 	}
 
-	return idParts, nil
+	return idParts[0], idParts[1], idParts[2], nil
 }
