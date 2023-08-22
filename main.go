48caf1387240d54f50b1a0f7cbfd78a7d1a52dde
Elie CHARRA <eliec@spacelift.io>
2023-08-22 17:44:16 +0200

    feat: add debug flag

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift"
@@ -10,7 +12,14 @@ var commit = "dev"
 var version = "dev"
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		ProviderAddr: "spacelift.io/spacelift-io/spacelift",
+		Debug:        debug,
 		ProviderFunc: spacelift.Provider(commit, version),
 	})
 }
