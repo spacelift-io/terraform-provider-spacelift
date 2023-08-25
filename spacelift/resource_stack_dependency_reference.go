a20a718688cea1e6b9a8cf3a5a488e97aada73f5
Piotr Truszkowski <truszkowski@gmail.com>
2023-08-25 13:46:21 +0200

    fix linter error

--- spacelift/resource_stack_dependency_reference.go
+++ spacelift/resource_stack_dependency_reference.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal"
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/structs"
 )
