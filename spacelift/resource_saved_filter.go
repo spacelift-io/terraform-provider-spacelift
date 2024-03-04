067ee231f3e2430e867b9447d26f1197cea26de3
Piotr Truszkowski <truszkowski@gmail.com>
2024-03-04 10:12:03 +0100

    adjust imports (reviewdog feedback)

--- spacelift/resource_saved_filter.go
+++ spacelift/resource_saved_filter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal"
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/structs"
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/validations"
