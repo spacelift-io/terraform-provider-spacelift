93de03e78b14b440ae21a64567b0b052623ed7e9
Thomas <tmeckel@users.noreply.github.com>
2024-03-06 13:33:54 +0000

    test: added TestRunResourceWait to spacelift/resource_run_test.go

--- spacelift/resource_run_test.go
+++ spacelift/resource_run_test.go
@@ -47,3 +47,50 @@ func TestRunResource(t *testing.T) {
 		})
 	})
 }
+
+func TestRunResourceWait(t *testing.T) {
+
+	t.Run("on a new stack", func(t *testing.T) {
+		const resourceName = "spacelift_run.test"
+
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		randomIDwp := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+
+		testSteps(t, []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				resource "spacelift_worker_pool" "test" {
+					name        = "Let's create a dummy worker pool to avoid running the job %s"
+				}
+
+				resource "spacelift_stack" "test" {
+					name           = "Test stack %s"
+					repository     = "demo"
+					branch         = "master"
+					worker_pool_id = spacelift_worker_pool.test.id
+				}
+
+				resource "spacelift_run" "test" {
+					stack_id = spacelift_stack.test.id
+
+					keepers = { "bacon" = "tasty" }
+
+					timeouts {
+						create = "30s"
+					}
+
+					wait {
+						enabled             = true
+						continue_on_timeout = true
+					}
+				}
+			`, randomIDwp, randomID),
+				Check: Resource(
+					resourceName,
+					Attribute("id", IsNotEmpty()),
+					Attribute("stack_id", Contains(randomID)),
+				),
+			},
+		})
+	})
+}
