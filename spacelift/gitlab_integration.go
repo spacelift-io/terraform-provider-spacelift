fbd47a6023c3607e8cbb22d741c876dc8be23626
Thomas Meckel <tmeckel@users.noreply.github.com>
2024-05-06 17:28:50 +0000

    refactor: moved GitLab schema field name constants to gitlab_integration.go

---
+++ spacelift/gitlab_integration.go
@@ -0,0 +1,16 @@
+package spacelift
+
+const (
+	gitLabID             = "id"
+	gitLabName           = "name"
+	gitLabDescription    = "description"
+	gitLabIsDefault      = "is_default"
+	gitLabLabels         = "labels"
+	gitLabSpaceID        = "space_id"
+	gitLabUserFacingHost = "user_facing_host"
+	gitLabAppID          = "app_id"
+	gitLabAPIHost        = "api_host"
+	gitLabToken          = "private_token"
+	gitLabWebhookURL     = "webhook_url"
+	gitLabWebhookSecret  = "webhook_secret"
+)
