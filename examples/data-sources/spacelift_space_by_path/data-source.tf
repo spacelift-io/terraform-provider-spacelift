ca7e11cd1cdcf1281144834e1eacc35af26106dc
Maciej Długoszek <muloni@gmail.com>
2024-10-24 14:56:40 +0200

    Fix docs

--- examples/data-sources/spacelift_space_by_path/data-source.tf
+++ examples/data-sources/spacelift_space_by_path/data-source.tf
@@ -6,7 +6,7 @@ output "space_description" {
   value = data.spacelift_space_by_path.space.description
 }
 
-// Assuming this data source is invoked in a run that belongs to a stack in a space located at "root", then the following data source shall be equal to the one above.
+// Assuming this data source is created in a run that belongs to a stack in a space located at following path - "root", then the following data source shall be equal to the one above.
 data "spacelift_space_by_relative_path" "space" {
   space_path = "second space/my space"
 }
