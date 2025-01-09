fa8d7d15f0207ca5b8ab954ae3b2ac863b0af279
Michal Romanowski <michal.rom089@gmail.com>
2025-01-09 13:44:36 +0100

    feat: add kubernetes_custom_workflow property for kubernetes schema

--- spacelift/internal/structs/stack_input.go
+++ spacelift/internal/structs/stack_input.go
@@ -65,7 +65,8 @@ type CloudFormationInput struct {
 // KubernetesInput represents Kubernetes-specific configuration.
 type KubernetesInput struct {
 	Namespace      graphql.String  `json:"namespace"`
-	KubectlVersion *graphql.String `json:"kubectlVersion"`
+	KubectlVersion *graphql.String `json:"kubectlVersionkubectlVersion"`
+	KubernetesWorkflowTool *graphql.String `json:"kubernetesWorkflowTool"`
 }
 
 // PulumiInput represents Pulumi-specific configuration.
