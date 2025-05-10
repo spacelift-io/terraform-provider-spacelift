b572c0846e03032dba2ab672fb05447c293d6e56
Marcin Wyszynski <marcin.pixie@gmail.com>
2025-05-10 19:58:31 +0200

    Add CIDRs to spacelift_ips data source

--- examples/data-sources/spacelift_ips/data-source.tf
+++ examples/data-sources/spacelift_ips/data-source.tf
@@ -1,1 +1,11 @@
 data "spacelift_ips" "ips" {}
+
+resource "aws_security_group_rule" "allow_spacelift" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = data.spacelift_ips.ips.cidrs
+  security_group_id = aws_security_group.example.id
+  description       = "Allow Spacelift IPs"
+}
