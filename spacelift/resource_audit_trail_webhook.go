b0a7305cd5f12ceb60b16752760b369deb3735ab
Marko Kos <markok@spacelift.io>
2026-02-06 10:30:01 +0100

    Removed ExactlyOneOf tag

--- spacelift/resource_audit_trail_webhook.go
+++ spacelift/resource_audit_trail_webhook.go
@@ -54,7 +54,6 @@ func resourceAuditTrailWebhook() *schema.Resource {
 				Description:      "`secret` is a secret that Spacelift will send with the request. Note that once it's created, it will be just an empty string in the state due to security reasons.",
 				DiffSuppressFunc: ignoreOnceCreated,
 				ConflictsWith:    []string{"secret_wo", "secret_wo_version"},
-				ExactlyOneOf:     []string{"secret", "secret_wo"},
 			},
 			"secret_wo": {
 				Type:          schema.TypeString,
@@ -63,7 +62,6 @@ func resourceAuditTrailWebhook() *schema.Resource {
 				Optional:      true,
 				WriteOnly:     true,
 				ConflictsWith: []string{"secret"},
-				ExactlyOneOf:  []string{"secret", "secret_wo"},
 				RequiredWith:  []string{"secret_wo_version"},
 			},
 			"secret_wo_version": {
