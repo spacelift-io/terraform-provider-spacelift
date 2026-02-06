c18c57a87d81c40ec09dc602ac2bfee5c26baa60
Marko Kos <markok@spacelift.io>
2026-02-06 12:02:18 +0100

    Added write-only fields for content of mounted_file resource and also added tests, fixed some of the description for audit_trail_webhook resource

--- spacelift/resource_mounted_file.go
+++ spacelift/resource_mounted_file.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
