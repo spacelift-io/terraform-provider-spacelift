25475b7f32047df35fb08507a702bc6883e43567
Marko Kos <markok@spacelift.io>
2026-02-06 10:24:15 +0100

    Generated the docs

--- examples/resources/spacelift_audit_trail_webhook/resource.tf
+++ examples/resources/spacelift_audit_trail_webhook/resource.tf
@@ -5,8 +5,8 @@ resource "spacelift_audit_trail_webhook" "example" {
 }
 
 resource "spacelift_audit_trail_webhook" "write_only_example" {
-  endpoint           = "https://example.com"
-  enabled            = true
-  secret_wo          = "somesupersecretkey"
-  secret_wo_versin   = 1
+  endpoint         = "https://example.com"
+  enabled          = true
+  secret_wo        = "somesupersecretkey"
+  secret_wo_versin = 1
 }
