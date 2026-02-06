e8824d591545d3d1602b1f82c2c271c4f013d723
Marko Kos <markok@spacelift.io>
2026-02-06 14:33:13 +0100

    Extracted write-only field validation function to a seperate file

--- spacelift/resource_audit_trail_webhook.go
+++ spacelift/resource_audit_trail_webhook.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal"
 	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/structs"
+	"github.com/spacelift-io/terraform-provider-spacelift/spacelift/internal/validations"
 )
 
 func resourceAuditTrailWebhook() *schema.Resource {
--- spacelift/resource_mounted_file.go
+++ spacelift/resource_mounted_file.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
