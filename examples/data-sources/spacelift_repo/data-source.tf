e655ab159a041f45e408bf85883076f7e5452e87
Jakub Wójtowicz <jakubw@spacelift.io>
2026-07-28 13:25:04 +0200

    docs(repos): drop the stack block from the data source example
    Attaching a repo to a stack arrives in a later change, so the generated page
    documented the data source with a config a reader could not apply.

--- examples/data-sources/spacelift_repo/data-source.tf
+++ examples/data-sources/spacelift_repo/data-source.tf
@@ -2,15 +2,6 @@ data "spacelift_repo" "this" {
   repo_id = "my-repo"
 }
 
-# Repos have no branches, so the branch is always "main" and the stack tracks
-# the latest commit.
-resource "spacelift_stack" "this" {
-  name       = "my-stack"
-  repository = data.spacelift_repo.this.repo_id
-  branch     = "main"
-  space_id   = data.spacelift_repo.this.space_id
-
-  spacelift {
-    id = data.spacelift_repo.this.repo_id
-  }
+output "repo_space" {
+  value = data.spacelift_repo.this.space_id
 }
